c816194495e4617fa9f19cf51c27d628864813f8
juniantowicaksono06 <juniantowicakson06@gmail.com>
2022-06-09 10:59:30 +0700

    Create Login Form

---
+++ components/login/Main.vue
@@ -0,0 +1,67 @@
+<style>
+    @import '../assets/styles/login.css';
+</style>
+<template>
+    <div class="container-fluid h-100 w-100" id="content_wrap">
+        <div class="position-relative h-100 w-100">
+            <div id="login_panel">
+                <form method="POST" action="#">
+                    <div class="row w-100 h-100" style="">
+                        <div class="col-12 col-sm-12 col-md-6 offset-md-3 col-lg-4 offset-lg-4 w-100 pr-0">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h5 class="text-center">Login</h5>
+                                </div>
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label class="login-text">User Domain</label>
+                                        <div class="input-group">
+                                            <input v-model="username_input" type="text" class="form-control" placeholder="User Domain..." autofocus>
+                                            <div class="input-group-append">
+                                                <div class="btn btn-primary login-normal-cursor">
+                                                    <font-awesome-icon :icon="['fa', 'user']"/>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="login-text">Password</label>
+                                        <div class="input-group">
+                                            <input v-model="password_input" type="password" class="form-control" placeholder="Password..." autofocus>
+                                            <div class="input-group-append">
+                                                <div class="btn btn-primary login-normal-cursor">
+                                                    <font-awesome-icon :icon="['fa', 'lock']"/>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group justify-content-between">
+                                            <div class="input-group-prepend ml-4">
+                                                <input type="checkbox" class="form-check-input" v-model="remember_me_input">
+                                                <label class="form-check-label">Remember me</label>
+                                            </div>
+                                            <button type="submit" class="btn btn-primary">Sign In</button>
+                                        </div>
+                                    </div>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                username_input: '',
+                password_input: '',
+                remember_me_input: false
+            }
+        }
+    }
+</script>
