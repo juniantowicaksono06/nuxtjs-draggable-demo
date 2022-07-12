6678e0bbf347721f4785bc770249d554bc92bea9
juniantowicaksono06 <juniantowicakson06@gmail.com>
2022-07-12 15:36:49 +0700

    Fix error workspace blank page

--- plugins/global.js
+++ plugins/global.js
@@ -1,5 +1,6 @@
 export default ({ app }, inject) => {
     inject('isValidUrl', (urlString) => {
+        if(typeof urlString == 'undefined') return false
         var pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
         return !!pattern.test(urlString.toLowerCase());
     })
